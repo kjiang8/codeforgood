878b97de36db4671ed21af7cc30f2087d771fa61
Leon Cheng <57leonardo@gmail.com>
2017-01-30 16:13:44 -0500

    started working on generating table per eventname

--- public/js/table.js
+++ public/js/table.js
@@ -49,64 +49,86 @@ rootRef.once("value").then( function(snapshot){
 		table += '</tr>';
 	});
 
-	$('table').append(table);
+	$('#example').append(table);
 
 
+
+
+  $.each(data.Events, function(key, value){
+    var moreTables = " \
+  <div class='container'> \
+    <div class='panel panel-primary'> \
+      <div class='panel-heading'> \
+        <h3 class='panel-title'>" + key + "</h3> \
+      </div> \
+      <table id='event_table' class='display' cellspacing='0' width='100%'> \
+        <thead> \
+          <tr> \
+            <th>First Name</th> \
+            <th>Last Name</th> \
+            <th>Position</th> \
+          </tr> \
+        </thead> \
+      </table> \
+    </div> \
+  </div>"
+    $('#more-tables').append( moreTables );
+  });
 
 });
 
 
- //creating the table :D
-
- (function(){
-     'use strict';
- 	var $ = jQuery;
- 	$.fn.extend({
- 		filterTable: function(){
- 			return this.each(function(){
- 				$(this).on('keyup', function(e){
- 					$('.filterTable_no_results').remove();
- 					var $this = $(this),
-                         search = $this.val().toLowerCase(),
-                         target = $this.attr('data-filters'),
-                         $target = $(target),
-                         $rows = $target.find('tbody tr');
-
- 					if(search == '') {
- 						$rows.show();
- 					} else {
- 						$rows.each(function(){
- 							var $this = $(this);
- 							$this.text().toLowerCase().indexOf(search) === -1 ? $this.hide() : $this.show();
- 						})
- 						if($target.find('tbody tr:visible').size() === 0) {
- 							var col_count = $target.find('tr').first().find('td').size();
- 							var no_results = $('<tr class="filterTable_no_results"><td colspan="'+col_count+'">No results found</td></tr>')
- 							$target.find('tbody').append(no_results);
- 						}
- 					}
- 				});
- 			});
- 		}
- 	});
- 	$('[data-action="filter"]').filterTable();
- })(jQuery);
-
- $(function(){
-     // attach table filter plugin to inputs
- 	$('[data-action="filter"]').filterTable();
-
- 	$('.container').on('click', '.panel-heading span.filter', function(e){
- 		var $this = $(this),
- 			$panel = $this.parents('.panel');
-
- 		$panel.find('.panel-body').slideToggle();
- 		if($this.css('display') != 'none') {
- 			$panel.find('.panel-body input').focus();
- 		}
- 	});
- 	//$('[data-toggle="tooltip"]').tooltip();
- })
+ // //creating the table :D
+ //
+ // (function(){
+ //     'use strict';
+ // 	var $ = jQuery;
+ // 	$.fn.extend({
+ // 		filterTable: function(){
+ // 			return this.each(function(){
+ // 				$(this).on('keyup', function(e){
+ // 					$('.filterTable_no_results').remove();
+ // 					var $this = $(this),
+ //                         search = $this.val().toLowerCase(),
+ //                         target = $this.attr('data-filters'),
+ //                         $target = $(target),
+ //                         $rows = $target.find('tbody tr');
+ //
+ // 					if(search == '') {
+ // 						$rows.show();
+ // 					} else {
+ // 						$rows.each(function(){
+ // 							var $this = $(this);
+ // 							$this.text().toLowerCase().indexOf(search) === -1 ? $this.hide() : $this.show();
+ // 						})
+ // 						if($target.find('tbody tr:visible').size() === 0) {
+ // 							var col_count = $target.find('tr').first().find('td').size();
+ // 							var no_results = $('<tr class="filterTable_no_results"><td colspan="'+col_count+'">No results found</td></tr>')
+ // 							$target.find('tbody').append(no_results);
+ // 						}
+ // 					}
+ // 				});
+ // 			});
+ // 		}
+ // 	});
+ // 	$('[data-action="filter"]').filterTable();
+ // })(jQuery);
+ //
+ // $(function(){
+ //     // attach table filter plugin to inputs
+ // 	$('[data-action="filter"]').filterTable();
+ //
+ // 	$('.container').on('click', '.panel-heading span.filter', function(e){
+ // 		var $this = $(this),
+ // 			$panel = $this.parents('.panel');
+ //
+ // 		$panel.find('.panel-body').slideToggle();
+ // 		if($this.css('display') != 'none') {
+ // 			$panel.find('.panel-body input').focus();
+ // 		}
+ // 	});
+ // 	//$('[data-toggle="tooltip"]').tooltip();
+ // })
 
 
 
